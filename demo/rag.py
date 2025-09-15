7ddd9a7515e87cbad4fd18ee4bae757dcc94111b
SAYAN02-DEV <sayan2003.dev@gmail.com>
2025-09-16 00:24:18 +0530

    CHANGED OPENAI TO GEMINI

--- demo/rag.py
+++ demo/rag.py
@@ -2,7 +2,6 @@ import os
 
 from dotenv import load_dotenv
 from llama_index.chat_engine.condense_plus_context import CondensePlusContextChatEngine
-from llama_index.llms.openai import OpenAI
 from llama_index.llms.types import ChatMessage, MessageRole
 from llama_index.query_engine import RetrieverQueryEngine
 from llama_index.retrievers import PathwayRetriever
@@ -10,15 +9,18 @@ from traceloop.sdk import Traceloop
 
 from pathway.xpacks.llm.vector_store import VectorStoreClient
 
+# Gemini imports
+import google.generativeai as genai
+from llama_index.llms.gemini import Gemini
+
+# load environment variables
 load_dotenv()
 
-
+# initialize Traceloop
 Traceloop.init(app_name=os.environ.get("APP_NAME", "PW - LlamaIndex (Streamlit)"))
 
 DEFAULT_PATHWAY_HOST = "demo-document-indexing.pathway.stream"
-
 PATHWAY_HOST = os.environ.get("PATHWAY_HOST", DEFAULT_PATHWAY_HOST)
-
 PATHWAY_PORT = int(os.environ.get("PATHWAY_PORT", "80"))
 
 
@@ -30,30 +32,36 @@ def get_additional_headers():
     return headers
 
 
+# vector store client
 vector_client = VectorStoreClient(
     PATHWAY_HOST,
     PATHWAY_PORT,
     additional_headers=get_additional_headers(),
 )
 
-
+# retriever
 retriever = PathwayRetriever(host=PATHWAY_HOST, port=PATHWAY_PORT)
 retriever.client = VectorStoreClient(
     host=PATHWAY_HOST, port=PATHWAY_PORT, additional_headers=get_additional_headers()
 )
 
-llm = OpenAI(model="gpt-3.5-turbo")
+# configure Gemini
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+llm = Gemini(model="models/gemini-1.5-flash")
 
+# retriever query engine
 query_engine = RetrieverQueryEngine.from_args(
     retriever,
 )
 
+# default chat history
 pathway_explaination = "Pathway is a high-throughput, low-latency data processing framework that handles live data & streaming for you."
 DEFAULT_MESSAGES = [
     ChatMessage(role=MessageRole.USER, content="What is Pathway?"),
     ChatMessage(role=MessageRole.ASSISTANT, content=pathway_explaination),
 ]
 
+# chat engine using Gemini
 chat_engine = CondensePlusContextChatEngine.from_defaults(
     retriever=retriever,
     system_prompt="""You are RAG AI that answers users questions based on provided sources.
